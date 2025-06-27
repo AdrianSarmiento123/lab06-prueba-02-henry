b06c54ab21ca2e8f810cb8393827dde1303a128d
AdrianSarmiento123 <henryadriansarmiento@gmail.com>
2025-06-27 10:55:17 -0500

    Update ShoppingCarttest.java

--- src/ShoppingCarttest.java
+++ src/ShoppingCarttest.java
@@ -16,41 +16,64 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author adria
  */
 public class ShoppingCarttest {
-    
-    public ShoppingCarttest() {
-    }
 
-    @org.junit.jupiter.api.BeforeAll
-    public static void setUpClass() throws Exception {
-    }
+  private ShoppingCart cart;
 
-    @org.junit.jupiter.api.AfterAll
-    public static void tearDownClass() throws Exception {
-    }
+   
 
-    @org.junit.jupiter.api.BeforeEach
-    public void setUp() throws Exception {
-    }
+  public ShoppingCarttest() {
 
-    @org.junit.jupiter.api.AfterEach
-    public void tearDown() throws Exception {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
-    @BeforeEach
-    public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
-    }
+    this.cart = new ShoppingCart();
+
+  }
+
+   
+
+  @Test
+
+  public void addProduct_con_precio_negativo () {
+
+    //ShoppingCart cart = new ShoppingCart();
+
+    cart.addProduct(10.0);
+
+    cart.addProduct(20.0);
+
+    assertEquals(2,cart.getProductCount());
+
+    assertEquals(30.0,cart.getTotal(),0.001);
+
+  }
+
+   
+
+  @Test
+
+    public void addProduct_con_precio_negativo_unhappy () {
+
+    //ShoppingCart cart = new ShoppingCart();
+
+    Exception exception = assertThrows(IllegalArgumentException.class,()->{
+
+       
+
+     cart.addProduct(00.0);  
+
+       
+
+    });
+
+    //cart.addProduct(20.0);
+
+    assertEquals("Price must be positive",exception.getMessage());
+
+    //assertEquals(30.0,cart.getTotal(),0.001);
+
+  }
+
+
+
+}
 
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
